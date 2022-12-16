19974337874c51c201ea5236135fc8aa0f6c3d02
mani1511 <krishmani453@gmail.com>
2022-12-16 15:39:06 +0530

    finishing in blog page and start in Function Key 7Function Key 7about Function Key 7usFunction Key 7 Function Key 7oFunction Key page

---
+++ selten/UI/about.php
@@ -0,0 +1,121 @@
+<?php
+include_once('header.php');
+?>
+
+<body>
+    <div class="row">
+        <div class="col-md-6">
+            <h2 class="topic ">Get to know about us and relive our journey</h2>
+            <h4 class="topic2">Get acquainted with our team and understand how we work and<br> proceed towards success.<h4>
+                    <!-- <img src="..\images\banners\starrating-removebg-preview.png" class="rating" alt=""><h6>(4.8 out of 5 ratings)</h6> -->
+                    <div class="star-icon">
+                        <i class="fa-solid fa-star "></i><i class="fa-solid fa-star "></i><i class="fa-solid fa-star "></i><i class="fa-solid fa-star "></i><i class="fa-solid fa-star-half-stroke"></i>
+                        <h6>(4.5 out of 5 ratings)</h6>
+                    </div>
+        </div>
+
+
+        <div class="col-md-6">
+            <img src="..\images\banners\aboutimg.jpg" class="top-img " alt="">
+        </div>
+    </div>
+
+
+
+    <div class="conatiner">
+        <div class="row main">
+            <div class="col-md-6 techrow">
+                <div class="row ">
+                    <h5 class="ourval">our values</h5>
+                    <div class="col-md-4">
+                        <img src="..\images\logo\Integrity-icon.jpg" class="inte " alt="">
+                        <p class="inte-para">Our first principal is honesty, which helps us meet the expectations we create.</p>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="..\images\logo\passion4-removebg-preview.png" class="passi animate__animated animate__flip" alt="">
+                        <p class="pass-para">Passion drives us to perform better,<br> progress continually,<br> and deliver timely.</p>
+                    </div>
+                    <div class="col-md-2">
+                        <img src="..\images\logo\learning-removebg-preview.png" class="learn" alt="">
+                        <p class="learn-para">Our team is always learning something new to get better at what they do</p>
+                    </div>
+
+                    <div class="col-md-2">
+                        <img src="..\images\logo\teamwork-removebg-preview.png" class="team" alt="">
+                        <p class="team-para">Our team members are all about helping each other.</p>
+                    </div>
+
+
+                </div>
+            </div>
+            <div>
+                <h4 class="ourgoal">our goal statement</h4>
+                <p class="our-para">The selten family believes in action, progress, and growth. The young startup has achieved several milestones in its active years till now, but nothing keeps us from seeking more and more. Since more and more people seem to indulge in video, we aim to break our own record in hours of video produced. That's not all, we are also determined to become HubSpot Elite Partners by the end of 2022.</p>
+            </div>
+            <!-- our journey -->
+            <section class="page-section" id="about">
+                <div class="container">
+                    <div class="text-center">
+                        <h2 class="section-heading text-uppercase">About</h2>
+                        <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                    </div>
+                    <ul class="timeline">
+                        <li>
+                            <div class="timeline-image">ihiu</div>
+                            <div class="timeline-panel">
+                                <div class="timeline-body">
+                                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="timeline-inverted">
+                            <div class="timeline-image"><img class="rounded-circle img-fluid" src="..\images\logo\yearnew21-removebg-preview.png" alt="..."></div>
+                            <div class="timeline-panel">
+                               
+                                <div class="timeline-body">
+                                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="timeline-image"><img class="rounded-circle img-fluid" src="..\images\logo\yearnew22-removebg-preview.png" alt="..."></div>
+                            <div class="timeline-panel">
+                                
+                                <div class="timeline-body">
+                                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="timeline-inverted">
+                            <div class="timeline-image"><img class="rounded-circle img-fluid" src="..\images\logo\yearnew23-removebg-preview.png" alt="..."></div>
+                            <div class="timeline-panel">
+                               
+                                <div class="timeline-body">
+                                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="timeline-inverted">
+                            <div class="timeline-image">
+                                <h4>
+                                    Be Part
+                                    <br>
+                                    Of Our
+                                    <br>
+                                    Story!
+                                </h4>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </div>
+    </div>
+
+
+</body>
+<?php
+include_once('footer.php');
+?>
+
+</html>
