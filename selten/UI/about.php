1d24b0a199e0d80af50650b64174f75bf0f8836a
mani1511 <krishmani453@gmail.com>
2022-12-27 20:34:14 +0530

    our-vision

--- selten/UI/about.php
+++ selten/UI/about.php
@@ -4,21 +4,21 @@ include_once('header.php');
 
 <body>
     <div class="row about-main">
-        <div class="col-md-6 top-col">
+        <div class="div">
+            <img src="..\images\banners\about us.jpg" class="banner-about" alt="">
+        </div>
+        <!-- <div class="col-md-6 top-col">
             <h2 class="topic ">Get to know about us and relive our journey</h2>
             <h4 class="topic2">Get acquainted with our team and understand how we work and<br> proceed towards success.<h4>
                     <!-- <img src="..\images\banners\starrating-removebg-preview.png" class="rating" alt=""><h6>(4.8 out of 5 ratings)</h6> -->
-                    <div class="starate-icon">
+                    <!-- <div class="starate-icon">
                         <i class="fa-solid fa-star "></i><i class="fa-solid fa-star "></i><i class="fa-solid fa-star "></i><i class="fa-solid fa-star "></i><i class="fa-solid fa-star-half-stroke"></i>
                         <h6>(4.5 out of 5 ratings)</h6>
                     </div>
-        </div>
+        </div> --> 
 
 
-        <div class="col-md-6">
-            <img src="..\images\images\a-banner.jpeg" class="top-img " alt="">
-        </div>
-    </div>
+      
 
 
 
@@ -48,22 +48,22 @@ include_once('header.php');
                     </div>
  -->
 
-  <div class="col-md-3 inte-head" style="background-image:linear-gradient(147deg, #1c92d2 2%, #f2fcfe 74%);">
+  <div class="col-md-3 inte-head" style="background-image:linear-gradient(147deg, #bdc3c7 2%,  #828280 74% );">
   <center> <img src="..\images\images\a-integrative-removebg-preview.png" class="new-inte" alt=""></center>
    
     <p class="new-para1">Our first principal is honesty, which helps us meet the expectations we create.</p>
   </div>
-  <div class="col-md-3 passi-head" style="background-image:linear-gradient(147deg, #1c92d2 2%, #f2fcfe 74%);">
+  <div class="col-md-3 passi-head" style="background-image:linear-gradient(147deg,#bdc3c7 2%,  #828280 74% );">
   <center> <img src="..\images\images\a-passion.png" class="new-passi" alt=""></center>
    
    <p class="new-para2">Passion drives us to perform better, progress continually, and deliver timely.</p>
   </div>
-  <div class="col-md-3 learn-head" style="background-image:linear-gradient(147deg, #83c5e7 2%,  #f2fcfe 74%)">
+  <div class="col-md-3 learn-head" style="background-image:linear-gradient(147deg, #bdc3c7 2%,  #828280 74%)">
   <center> <img src="..\images\images\a-elearn.png" class="new-learn" alt=""></center>
    
    <p class="new-para3">learn-para">Our team is always learning something new to get better at what they do.</p>
   </div>
-  <div class="col-md-3 team-head" style="background-image:linear-gradient(147deg, #1c92d2 2%, #f2fcfe 74%);">
+  <div class="col-md-3 team-head" style="background-image:linear-gradient(147deg,#bdc3c7 2%,  #828280 74% );">
   <center> <img src="..\images\images\a-team.png" class="new-team" alt=""></center>
    
    <p class="new-para4">team-para">Our team members are all about helping each other</p>
@@ -75,13 +75,29 @@ include_once('header.php');
             </div>
             <!-- our goal -->
             <div class="row">
-                <div class="col-md-6">
-                  <img src="..\images\images\employeewomen-removebg-preview.png" class="goal-img" alt="">
+                <div class="col-md-6 ">
+                  <img src="..\images\images\employeeeee.webp" class="goal-img" alt="">
                 </div>
                 <div class="col-md-6 ourgoal-right">
                     <h6 class="our-our">our</h6>
                     <h4 class="our-goal"> goal <br>statement</h4>
                     <p class="goal-para">The selten family believes in action, progress, and growth. The young startup has achieved several milestones in its active years till now, but nothing keeps us from seeking more and more. Since more and more people seem to indulge in video, we aim to break our own record in hours of video produced. That's not all, we are also determined to become HubSpot Elite Partners by the end of 2022.</p>
+                </div>
+            </div>
+            <!-- our mission -->
+            <div class="row">
+                <div class="col-md-6">
+                    
+                    <img src="..\images\logo\roundarrow-removebg-preview.png" class="right-img" alt="">
+                   
+                    <h4 class="our-miss">our mission</h4>
+                    <p class="miss-para">our mission of young minds <br>showcasing our talent to the CRM<br> and Marketing world to ease <br>the processes of clients business<br> and have happy customers.<br> We offer services on various platforms<br> across the globe which includes small,<br> medium and large scale business. </p>
+                    <img src="..\images\logo\greenarrow-removebg-preview.png" class="left-img" alt="">
+                    <h4 class="our-viss">our vission</h4>
+                    <p class="viss-para">our vission of young minds <br>showcasing our talent to the CRM<br> and Marketing world to ease <br>the processes of clients business<br> and have happy customers.<br> We offer services on various platforms<br> across the globe which includes small,<br> medium and large scale business. </p>
+                </div>
+                <div class="col-md-6">
+                    <img src="..\" class="pexels-img" alt="">
                 </div>
             </div>
             
