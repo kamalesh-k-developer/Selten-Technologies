ce344d7907e45080ec7062a80930c62eb9a0de66
karthick2408 <karthickraj240800@gmail.com>
2023-01-02 20:21:23 +0530

    start work with service link page

--- selten/UI/about.php
+++ selten/UI/about.php
@@ -2,129 +2,167 @@
 include_once('header.php');
 ?>
 
+
+
 <body>
-    <div class="row about-main">
-        <div class="about-high">
-            <img src="..\images\banners\about us.jpg" class="ban-aboutus" alt="">
-        </div>
-
-        <div class="conatiner-about">
-            <div class="row main-about">
-                <div class="col-md-6 techrow animate__animated animate__fadeInDown">
-                    <div class="row techrow2 ">
-                        <h5 class="ourval">our values</h5>
-                        <div class="col-md-3 inte-head">
-                            <center> <img src="..\images\images\a-integrative-removebg-preview.png" class="new-inte" alt=""></center>
-
-                            <p class="new-para1">Our first principal is honesty, which helps us meet the expectations we create.</p>
-                        </div>
-                        <div class="col-md-3 passi-head" style="background-image:linear-gradient(147deg,#B8D3FE 2%,  #AECAD6 74%);">
-                            <center> <img src="..\images\images\a-passion.png" class="new-passi" alt=""></center>
-
-                            <p class="new-para2">Passion drives us to perform better, progress continually, and deliver timely.</p>
-                        </div>
-                        <div class="col-md-3 learn-head" style="background-image:linear-gradient(147deg, #B8D3FE 2%,  #AECAD6 74%)">
-                            <center> <img src="..\images\images\a-elearn.png" class="new-learn" alt=""></center>
-
-                            <p class="new-para3">learn-para">Our team is always learning something new to get better at what they do.</p>
-                        </div>
-                        <div class="col-md-3 team-head" style="background-image:linear-gradient(147deg,#AACAEF 2%,  #FDE7F9 74% );">
-                            <center> <img src="..\images\images\a-team.png" class="new-team" alt=""></center>
-
-                            <p class="new-para4">team-para">Our team members are all about helping each other</p>
-                        </div>
-                    </div>
-
-                </div>
-
-            </div>
-
-            <!-- our goal -->
-            <div class="row">
-                <div class="col-md-6 ">
-                    <img src="..\images\images\employeeeee.webp" class="goal-img" alt="">
-                </div>
-                <div class="col-md-6 ourgoal-right">
-                    <h6 class="our-our">our</h6>
-                    <h4 class="our-goal"> goal <br>statement</h4>
-                    <p class="goal-para">The selten family believes in action, progress, and growth. The young startup has achieved several milestones in its active years till now, but nothing keeps us from seeking more and more. Since more and more people seem to indulge in video, we aim to break our own record in hours of video produced. That's not all, we are also determined to become HubSpot Elite Partners by the end of 2022.</p>
-                </div>
-            </div>
-            
-            <!-- our mission -->
-            <div class="row">
-                <div class="col-md-6">
-                    <img src="..\images\images\brain3.jpg" class="pexels-img" alt="">
-                </div>
-                <div class="col-md-6">
-                    <h4 class="our-miss">our mission</h4> <img src="..\images\logo\down-removebg-preview.png" class="down-img" alt="">
-                    <p class="miss-para">our mission of young minds <br>showcasing our talent to<br> the CRM and Marketing world</p>
-                    <i class="fa-regular fa-circle full-circle"></i>
-                    <h4 class="our-viss">our vision</h4>
-                    <p class="viss-para">our mission of young minds <br>showcasing our talent <br>to the CRM and Marketing world</p>
-
-
-                </div>
-            </div>
-
-            <!-- our journey -->
-            <section class="about-section animate__animated.animate__faster " id="about">
-                <div class="container-about">
-                    <div class="about-center">
-                        <h2 class="section-heading text-about">About</h2>
-                        <h3 class="section-subheading text-journey">Our Journey</h3>
-                    </div>
-                    <ul class="timeline">
-                        <li>
-                            <div class="timeline-image">
-                                <h5 class="t-journey">2020</h5>
-                            </div>
-                            <div class="timeline-panel">
-                                <div class="timeline-body">
-                                    <p class="journey-para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="timeline-inverted">
-                            <div class="timeline-image">
-                                <h5 class="t-journey">2021</h5>
-                            </div>
-                            <div class="timeline-panel">
-
-                                <div class="timeline-body">
-                                    <p class="journey-para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="timeline-image">
-                                <h5 class="t-journey">2022</h5>
-                            </div>
-                            <div class="timeline-panel">
-
-                                <div class="timeline-body">
-                                    <p class="journey-para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="timeline-inverted">
-                            <div class="timeline-image">
-                                <h5 class="t-journey">2023</h5>
-                            </div>
-                            <div class="timeline-panel">
-
-                                <div class="timeline-body">
-                                    <p class="journey-para">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
-                                </div>
-                            </div>
-                        </li>
-
-                    </ul>
-                </div>
-            </section>
-
-        </div>
+    <div class="div">
+        <img src="..\images\banners\about us.jpg" class="about-topi" alt="">
     </div>
+    <div class="container cont-high">
+      <div class="aboutBox">
+        <div class="icon-a" style="--i: #4eb7ff">
+        <i class="fa-sharp fa-solid fa-hands-asl-interpreting"></i>
+        </div>
+        <div class="content-first">
+          <h2>integrity</h2>
+          <p>
+          CRM services provided by Selten Technologies comes in three distinct modules: Marketing,
+                                    Sales, and Customer Service.
+          </p>
+        </div>
+      </div>
+      <div class="aboutBox">
+        <div class="icon-a" style="--i: #fd6494">
+        <i class="fa-solid fa-poo-storm"></i>
+        </div>
+        <div class="content-first">
+          <h2>passion</h2>
+          <p>
+          CRM services provided by Selten Technologies comes in three distinct modules: Marketing,
+                                    Sales, and Customer Service. 
+          </p>
+        </div>
+      </div>
+      <div class="aboutBox">
+        <div class="icon-a" style="--i: #43f390">
+        <i class="fa-brands fa-leanpub"></i>
+        </div>
+        <div class="content-first">
+          <h2>learning</h2>
+          <p>
+          CRM services provided by Selten Technologies comes in three distinct modules: Marketing,
+                                    Sales, and Customer Service. 
+          </p>
+        </div>
+      </div>
+      <div class="aboutBox">
+        <div class="icon-a" style="--i: #ffb508">
+        <i class="fa-solid fa-people-group"></i>
+        </div>
+        <div class="content-first">
+          <h2>team work</h2>
+          <p>
+          CRM services provided by Selten Technologies comes in three distinct modules: Marketing,
+                                    Sales, and Customer Service.
+          </p>
+        </div>
+      </div>
+      <div class="aboutBox">
+        <div class="icon-a" style="--i: #37ba82">
+        <i class="fa-sharp fa-solid fa-video"></i>
+        </div>
+        <div class="content-first">
+          <h2>Video Editing</h2>
+          <p>
+          CRM services provided by Selten Technologies comes in three distinct modules: Marketing,
+                                    Sales, and Customer Service.
+          </p>
+        </div>
+      </div>
+      <div class="aboutBox">
+        <div class="icon-a" style="--i: #cd57ff">
+        
+        </div>
+        <div class="content-first">
+          <h2>web Development</h2>
+          <p>
+          CRM services provided by Selten Technologies comes in three distinct modules: Marketing,
+                                    Sales, and Customer Service.
+          </p>
+        </div>
+      </div>
+    </div>
+    <!-- ..........ABOUT US............. -->
+    <!DOCTYPE html>
+
+    <div class="aboutus-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="aboutus">
+                        <h2 class="aboutus-title">About Us</h2>
+                        <p class="aboutus-text">Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in.</p>
+                        <p class="aboutus-text">This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem</p>
+                        <a class="aboutus-more" href="#">read more</a>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="aboutus-banner">
+                        <img src="http://themeinnovation.com/demo2/html/build-up/img/home1/about1.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-md-5 col-sm-6 col-xs-12">
+                    <div class="feature">
+                        <div class="feature-box">
+                            <div class="clearfix">
+                                <div class="iconset">
+                                <i class="fa-solid fa-pen-nib pen"></i>
+                                </div>
+                                <div class="feature-content">
+                                    <h4>Work with heart</h4>
+                                    <p>Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="feature-box">
+                            <div class="clearfix">
+                                <div class="iconset">
+                                <i class="fa-solid fa-share-from-square"></i>
+                                </div>
+                                <div class="feature-content">
+                                    <h4>Reliable services</h4>
+                                    <p>Donec vitae sapien ut libero venenatis faucibu. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="feature-box">
+                            <div class="clearfix">
+                                <div class="iconset">
+                                <i class="fa-solid fa-thumbs-up"></i>
+                                </div>
+                                <div class="feature-content">
+                                    <h4>Great support</h4>
+                                    <p>Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+<!-- .........OUR GOAL STATEMENT................ -->
+
+
+<div class="container">
+  <div class="aboutus-container">
+<div class="aboutus-cards">
+    <div class="aboutus-cards__image"></div>
+  
+    <div class="aboutus-cards__content">
+      <div class="aboutus-cards__content__items">
+      <span class="aboutus-cards__content__items__small-heading">OUR</span>
+      <h2 class="aboutus-cards__content__items__large-heading">GOAL STATEMENT</h2>
+      <p class="aboutus-cards__content__items__text">The selten family believes in action, progress, and growth. The young startup has achieved several milestones in its active years till now, but nothing keeps us from seeking more and more. Since more and more people seem to indulge in video, we aim to break our own record in hours of video produced. That's not all, we are also determined to become HubSpot Elite Partners by the end of 2022 That's not all, we are also determined to become HubSpot Elite Partners by the end of 2022</p>
+      </div>
+      <div class="aboutus-cards_content__button">Read more</div>
+    
+    </div>
+  </div>
+  </div>
+</div>
+
+ 
 
 
 </body>
